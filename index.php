3a61096a1115a39a452e0c383a878c23c0477342
DavidYS <DavidYS@yahoo.com>
2016-08-25 20:22:58 +0300

    Unsaved file? Possible error {
    Parse error: syntax error, unexpected end of file, expecting elseif (T_ELSEIF) or else (T_ELSE) or endif (T_ENDIF) in D:\Programare\UserLogin\index.php on line 79
    }

--- index.php
+++ index.php
@@ -14,70 +14,67 @@ if( isset($_SESSION['user_id']) ){
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Libra | Buy online</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
-</head>
-<body>
-
-	<header>
-		<ul>
-		<li><a href="/">Libra</a></li>
-		<li><a href="/"></a></li>
-		<li><a href="/"></a></li>
-		<li><a href="/"></a></li>
-		<li><a href="/"></a></li>
-		</ul>
-	<header>s
-    <div class="bg">
-	  <div class="logo"></div>
-	</div>
-	<?php if( !empty($user) ): ?>
-
 	<head>
 		<title>Libra | Buy online</title>
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-	<div class='cointainer-fluid'>
-			<header>
+		<header>
 			<ul>
 				<li><a href="/">Libra</a></li>
-				<li><a href="/">Home</a></li>
-				<li><a href="/">Books</a></li>
-				<?php
-					if( isset($_SESSION['user_id'])) { ?>
-				<li><a href='/'>Become an Admin</a></li>
-				<li><a href='logout.php'>Log Out</a></li>
-				<?php } else { ?>
-				<li><a href="/"></a>Login</li>
-				<li><a href="/"></a>Register</li>
-				<?php } ?>
+				<li><a href="/"></a></li>
+				<li><a href="/"></a></li>
+				<li><a href="/"></a></li>
+				<li><a href="/"></a></li>
 			</ul>
-		</header>
-		<div class="bg">
-			<div class="logo"></div>
-		</div>
-		<?php if( isset($_SESSION['user_id']) ) { ?>
-		<br />Welcome <?= $user['email']; ?>
-		<br /><br />You are successfully logged in!
-		<br /><br />
-		<a href="logout.php">Logout?</a>
-		<?php } else { ?>
-
-		<h1>Please Login or Register</h1>
-		<a href="login.php">Login</a> or
-		<a href="register.php">Register</a>
-		<?php } ?>
+			<header>s
+				<div class="bg">
+					<div class="logo"></div>
 				</div>
-		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src='js/functions.js'></script>
-		<script src='js/verify.js'></script>
-	</body>
-</html>
+				<?php if( !empty($user) ): ?>
+				<head>
+					<title>Libra | Buy online</title>
+					<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+					<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+					<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+				</head>
+				<body>
+					<div class='cointainer-fluid'>
+						<header>
+							<ul>
+								<li><a href="/">Libra</a></li>
+								<li><a href="/">Home</a></li>
+								<li><a href="/">Books</a></li>
+								<?php
+								if( isset($_SESSION['user_id'])) { ?>
+								<li><a href='/'>Become an Admin</a></li>
+								<li><a href='logout.php'>Log Out</a></li>
+								<?php } else { ?>
+								<li><a href="/"></a>Login</li>
+								<li><a href="/"></a>Register</li>
+								<?php } ?>
+							</ul>
+						</header>
+						<div class="bg">
+							<div class="logo"></div>
+						</div>
+						<?php if( isset($_SESSION['user_id']) ) { ?>
+						<br />Welcome <?= $user['email']; ?>
+						<br /><br />You are successfully logged in!
+						<br /><br />
+						<a href="logout.php">Logout?</a>
+						<?php } else { ?>
+						<h1>Please Login or Register</h1>
+						<a href="login.php">Login</a> or
+						<a href="register.php">Register</a>
+						<?php } ?>
 
+					</div>
+				</div>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+				<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+				<script src='js/functions.js'></script>
+				<script src='js/verify.js'></script>
+			</body>
+		</html>
