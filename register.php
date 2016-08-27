dbad491e9d14a16f1f1d1d3a229b1b8f3e53477e
DavidK <ghiurau.david@yahoo.com>
2016-08-27 12:12:38 +0300

    Added the Bootstrap navbar

--- register.php
+++ register.php
@@ -28,22 +28,57 @@ endif;
 	</head>
 	<body>
 	<div class='cointainer-fluid'>
-		<header>
-			<ul>
-				<li><a href="/">Libra</a></li>
-				<li><a href="/">Home</a></li>
-				<li><a href="/">Books</a></li>
-				<?php
-					if( isset($_SESSION['user_id'])) { ?>
-				<li><a href='/'>Become an Admin</a></li>
-				<li><a href='logout.php'>Log Out</a></li>
-				<?php } else { ?>
-				<li><a href="/"></a>Login</li>
-				<li><a href="/"></a>Register</li>
-				<?php } ?>
+		<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+       <a class="navbar-brand" href="#">
+        <img alt="Brand" src="assets/images/brand.jpg">
+    </div>
 
-			</ul>
-		</header>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Find Books <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Adventure</a></li>
+            <li><a href="#">Comedy</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Biography</a></li>
+          </ul>
+        </li>
+      </ul> 
+      
+ 		<?php if(!isset($_SESSION['user_id'])) { ?>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php">Login</a></li>
+        <li class='active'><a href="register.php">Register</a></li>
+      </ul>
+      <?php } else { ?>
+      	<ul>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Your Account <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">History</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+      <?php } ?>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 		<div class="bg">
 			<div class="logo"></div>
 		</div>
